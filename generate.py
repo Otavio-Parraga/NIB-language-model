ff28a85f5d5cd3785e1610c6470559507bd1c848
Otavio-Parraga <otavio.parraga@acad.pucrs.br>
2020-11-08 06:15:27 -0300

    generate method adapted

--- generate.py
+++ generate.py
@@ -1,78 +1,77 @@
-###############################################################################
-# Language Modeling on Wikitext-2
-#
-# This file generates new sentences sampled from the language model
-#
-###############################################################################
-
-import argparse
-
 import torch
-
+from models.LSTM import LSTMModel
+from models.AttentionLSTM import AttentionLSTMLanguageModel
+from models.Transformer import TransformerModel
 import data
+import argparse
+from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description='PyTorch Wikitext-2 Language Model')
+parser = argparse.ArgumentParser()
 
-# Model parameters.
-parser.add_argument('--data', type=str, default='./data/wikitext-2',
-                    help='location of the data corpus')
-parser.add_argument('--checkpoint', type=str, default='./model.pt',
-                    help='model checkpoint to use')
-parser.add_argument('--outf', type=str, default='generated.txt',
-                    help='output file for generated text')
-parser.add_argument('--words', type=int, default='1000',
-                    help='number of words to generate')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
-parser.add_argument('--cuda', action='store_true',
-                    help='use CUDA')
-parser.add_argument('--temperature', type=float, default=1.0,
-                    help='temperature - higher will increase diversity')
-parser.add_argument('--log-interval', type=int, default=100,
-                    help='reporting interval')
+parser.add_argument('--word', type=str, default=None)
+parser.add_argument('--model', type=str, default='lstm')
+parser.add_argument('--dataset', type=str, default='blogset')
+parser.add_argument('--temperature', type=float, default=1.)
+parser.add_argument('--limit', type=int, default=1000)
+parser.add_argument('--bias_reg', type=str, default=None)
+parser.add_argument('--number', type=int, default=1)
+
 args = parser.parse_args()
+args.tied = True
 
-# Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
-if torch.cuda.is_available():
-    if not args.cuda:
-        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+FILE_NAME = f'{args.model}_{args.dataset}_{args.temperature}_{args.limit}.txt' if args.bias_reg == None else f'{args.model}_{args.dataset}_{args.temperature}_{args.limit}_bias_reg_{args.bias_reg}.txt' 
+MODEL_DIR = f'{args.model}/{args.dataset}/best-model.pt' if args.bias_reg ==None else f'{args.model}/{args.dataset}/bias_reg_{args.bias_reg}/best-model.pt' 
 
-device = torch.device("cuda" if args.cuda else "cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-if args.temperature < 1e-3:
-    parser.error("--temperature has to be greater or equal 1e-3")
+corpus = data.Corpus(f'./data/{args.dataset}')
 
-with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f).to(device)
+model = torch.load(f'checkpoints/{MODEL_DIR}').to(device)
+
+model_type =  model.model_type if hasattr(model, 'model_type') else 'lstm'
+
+if model_type != 'transformer':
+    hidden = model.init_hidden(1)
+
 model.eval()
 
-corpus = data.Corpus(args.data)
+idx2word = corpus.dictionary.idx2word
+word2idx = corpus.dictionary.word2idx
 ntokens = len(corpus.dictionary)
 
-is_transformer_model = hasattr(model, 'model_type') and model.model_type == 'Transformer'
-if not is_transformer_model:
-    hidden = model.init_hidden(1)
-input = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
+print(f'Runs: {args.number} | Model: {args.model} | Dataset: {args.dataset} | Bias_reg: {args.bias_reg} | Temperature: {args.temperature}')
+for j in tqdm(range(args.number)):
+    if args.word != None:
+        input_tensor = torch.tensor([[word2idx[args.word]]]).to(device)
+        
+    else:
+        input_tensor = torch.randint(ntokens, (1, 1), dtype=torch.long).to(device)
 
-with open(args.outf, 'w') as outf:
-    with torch.no_grad():  # no tracking history
-        for i in range(args.words):
-            if is_transformer_model:
-                output = model(input, False)
-                word_weights = output[-1].squeeze().div(args.temperature).exp().cpu()
-                word_idx = torch.multinomial(word_weights, 1)[0]
-                word_tensor = torch.Tensor([[word_idx]]).long().to(device)
-                input = torch.cat([input, word_tensor], 0)
-            else:
-                output, hidden = model(input, hidden)
+    final_string = f'{idx2word[input_tensor.item()]} '
+
+    i = 0
+
+    with torch.no_grad():
+        with open(f'generated_text/{FILE_NAME}', 'a+', encoding='utf-8') as f:
+            while True:
+                if model_type == 'transformer':
+                    output = model(input_tensor)
+
+                elif model_type == 'att_lstm':
+                    output, _ = model(input_tensor)
+
+                elif model_type == 'lstm':
+                    output, hidden = model(input_tensor, hidden)
+
                 word_weights = output.squeeze().div(args.temperature).exp().cpu()
                 word_idx = torch.multinomial(word_weights, 1)[0]
-                input.fill_(word_idx)
+                input_tensor.fill_(word_idx)
+                word = idx2word[word_idx]
 
-            word = corpus.dictionary.idx2word[word_idx]
-
-            outf.write(word + ('\n' if i % 20 == 19 else ' '))
-
-            if i % args.log_interval == 0:
-                print('| Generated {}/{} words'.format(i, args.words))
+                if i < args.limit:
+                    final_string = final_string + word + ' '
+                else:
+                    f.write(final_string + '\n\n')
+                    #print(final_string)
+                    break
+                i += 1
